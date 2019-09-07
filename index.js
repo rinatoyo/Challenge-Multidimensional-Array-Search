7c1559b6dff6fb4e1841f2e387cd8726c6dc4790
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-09-07 10:03:32 -1000

    challenge finished

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function locate(array, target) {
   // do work here
+  let hash = {};
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      hash[array[i][j]] = i;
+    }
+  }
+
+  if (hash.hasOwnProperty(target)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = locate;
